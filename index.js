25a260985bd3dbbf9290a8e2639a9568d0098a7b
Michael Hess <m_hess@gmx.net>
2023-02-17 08:04:09 +0100

    refactor mqtt client code into module

--- index.js
+++ index.js
@@ -1,38 +1,6 @@
-const mqtt = require('mqtt');
-
-const logger = require('./logger.js').logger;
+const { logger } = require('./logger.js');
+const mqtt = require('./mqtt_client.js');
 
 logger.debug('Starting....');
 
-const client = mqtt.connect("mqtt://192.168.3.46:1883");
-
-const topicName = 'evcc/loadpoints/1/mode';
-// connect to same client and subscribe to same topic name  
-client.on('connect', () => {
-    // can also accept objects in the form {'topic': qos} 
-    client.subscribe(topicName, (err, granted) => {
-        if (err) {
-            logger.error(err, 'err');
-        }
-        logger.info("connected");
-    })
-});
-
-// on receive message event, log the message to the console 
-client.on('message', (topic, message, packet) => {
-    logger.trace(packet);
-    if (topic === topicName) {
-        logger.debug(packet.payload.toString());
-    }
-});
-
-client.on("packetsend", (packet) => {
-    logger.trace(packet, 'packet2');
-}) 
-
-process.on('SIGINT', function() {
-    console.log('Got SIGINT.  Going to exit.');
-    //Your code to execute before process kill.
-    client.end();
-    process.kill(process.pid);
-});
+mqtt.connect();
