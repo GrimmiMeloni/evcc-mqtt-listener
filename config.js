6bb9f288ea22e39173b5d1431f274d4605ed4b89
Michael Hess <m_hess@gmx.net>
2023-02-19 09:19:45 +0100

    cleanup

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
 const config = require('config');
 
-//const mqtt_url = "mqtt://192.168.3.46:1883";
 const mqtt_url = config.get('mqtt.broker.url');
 exports.mqtt_url = mqtt_url;
 
-// const chargeModeTopic = 'evcc/loadpoints/1/mode';
 const chargeModeTopic = config.get('mqtt.topics.mode');
 exports.chargeModeTopic = chargeModeTopic;
 
-// const chargeStateTopic = 'evcc/loadpoints/1/charging';
 const chargeStateTopic = config.get('mqtt.topics.charging');
 exports.chargeStateTopic = chargeStateTopic;
